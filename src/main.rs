27a670cbf736a48684bf6102e5519c8224b91248
akosnad <akos@nadudvari.org>
2022-12-22 21:03:40 +0100

    build: ability to pass extra args to QEMU

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,31 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let debug: bool = args.contains(&String::from("-d")) || args.contains(&String::from("--debug"));
     let bios: bool = args.contains(&String::from("-b")) || args.contains(&String::from("--bios"));
-    
+    let mut extra_args = Vec::new();
+
+    // find extra arguments to pass to QEMU
+    if let Some((idx, _)) = args
+        .iter()
+        .enumerate()
+        .find(|(_, e)| **e == String::from("--"))
+    {
+        extra_args = args
+            .iter()
+            .skip(idx + 1)
+            .collect();
+    }
+
     if bios {
         let bios_image_path = Path::new(env!("BIOS_PATH"));
-        run_in_qemu(bios_image_path, None, debug);
+        run_in_qemu(bios_image_path, None, debug, extra_args);
     } else {
         let uefi_image_path = Path::new(env!("UEFI_PATH"));
         let omvf_path = &Path::new("OVMF-pure-efi.fd");
-        run_in_qemu(uefi_image_path, Some(omvf_path), debug);
+        run_in_qemu(uefi_image_path, Some(omvf_path), debug, extra_args);
     }
 }
 
-fn run_in_qemu(uefi_gpt_path: &Path, omvf_path: Option<&Path>, debug: bool) {
+fn run_in_qemu(uefi_gpt_path: &Path, omvf_path: Option<&Path>, debug: bool, extra_args: Vec<&String>) {
     let mut cmd = Command::new("qemu-system-x86_64");
     cmd.arg("-serial").arg("stdio");
     cmd.arg("-drive");
@@ -24,6 +37,7 @@ fn run_in_qemu(uefi_gpt_path: &Path, omvf_path: Option<&Path>, debug: bool) {
         cmd.arg("-bios").arg(omvf_path);
     }
     if debug { cmd.arg("-s").arg("-S"); }
+    for arg in extra_args { cmd.arg(arg); }
 
     let status = cmd.status().unwrap().code().or(Some(1)).unwrap();
     std::process::exit(status);
